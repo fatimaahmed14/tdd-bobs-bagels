7af34088e3c3afa424404b7ef631d17e74a95249
fatima <fxtimaahmed14@gmail.com>
2023-02-16 14:43:50 +0000

    basket refactored

--- src/basket.js
+++ src/basket.js
@@ -1,118 +1,62 @@
-// const data = require('../inventory.json')
-// const { inventory } = data
-// ^ need to implement real data + bob's actual bagels
+const data = require('../inventory.json')
 
-const Basket = []
-let capacity = 5
+const { inventory } = data
 
-function getBasket() {
-  return Basket
-}
+const basket = []
+let basketCapacity = 10
 
-function add(variant) {
-  const bagel = {
-    name: 'Bagel',
-    variant,
-    quantity: 1,
-    price: 0.5
-  }
-  const found = Basket.find((item) => item.variant === bagel.variant)
-
-  if (found) {
-    return (found.quantity += 1)
-  }
-
-  return Basket.push(bagel)
-}
-
-// need to increase the quantity every time a bagel of the same sku is added
-// need to use find() then increase quantity property by one is it already exists
-// if it doesnt exist push it in the basket
-// const found = Basket.find((item) => item.variant === bagel)
-
-// if (!found) {
-//   return Basket.push(bagel)
-// }
-// found.quantity += 1
-//         return found
-
-function resetBasket() {
-  Basket.splice(0, Basket.length)
-}
-
-function increaseBasketCapacity(newCapacity) {
-  return (capacity = newCapacity)
-}
-
-function isFull() {
-  const basketCapacity = Basket.length
-  if (basketCapacity <= capacity) {
-    return true
-  } else {
+function add(sku) {
+  if (basket.length >= basketCapacity) {
     return false
   }
-}
-
-function remove(bagel) {
-  const found = Basket.find((item) => item.variant === bagel)
-
-  if (!found) {
-    return false
+  const findBagel = basket.find((bagel) => bagel.sku === sku)
+  if (findBagel) {
+    findBagel.quantity += 1
+    return findBagel
   }
 
-  const index = Basket.findIndex((item) => item.variant === bagel)
-  Basket.splice(index, 1)
-  return found
+  const bagelAdd = inventory.find((bagel) => bagel.sku === sku)
+  const copyBagel = { ...bagelAdd, quantity: 1 }
+  basket.push(copyBagel)
+  return copyBagel
 }
 
-function displayPrice(bagel) {
-  const found = Basket.find((item) => item.variant === bagel)
-
-  if (!found) {
-    return false
-  }
-  return found.price
-}
-
-function total() {
-  return Basket.reduce((runningTotal, bagel) => {
-    return runningTotal + bagel.price
+function totalBagelPrice() {
+  return basket.reduce((total, bagel) => {
+    return total + Number(bagel.price) * bagel.quantity
   }, 0)
+}
+
+function remove(sku) {
+  const findBagel = basket.find((bagel) => bagel.sku === sku)
+
+  if (!findBagel) {
+    return false
+  }
+
+  const index = basket.findIndex((bagel) => bagel.sku === sku)
+  basket.splice(index, 1)
+  return findBagel
+}
+
+function returnBasket() {
+  return [...basket]
+}
+
+function basketReset() {
+  basket.splice(0, basket.length)
+}
+
+function theCapacity(num) {
+  basketCapacity = num
+  basket.splice(0, basket.length)
 }
 
 module.exports = {
   add,
-  resetBasket,
-  isFull,
-  increaseBasketCapacity,
+  basketReset,
   remove,
-  getBasket,
-  displayPrice,
-  total
+  returnBasket,
+  totalBagelPrice,
+  theCapacity
 }
-
-// class Basket {
-//     basket = []
-
-//     constructor(capacity = 10) {
-//         this.capacity = capacity
-//     }
-
-//     add(sku) {
-//         if (this.basket.length >= this.capacity) {
-//             return false
-//         }
-
-//         const found = this.basket.find(bagel => bagel.sku === sku)
-
-//         if (found) {
-//             found.quantity += 1
-//             return found
-//         }
-
-//         const bagelToAdd = inventory.find(bagel => bagel.sku === sku)
-//         const newBagel = {...bagelToAdd, quantity: 1}
-//         this.basket.push(newBagel)
-//         return newBagel
-//     }
-// }
